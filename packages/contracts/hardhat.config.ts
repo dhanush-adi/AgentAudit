a62ea2c735dded8a0bc1867e7433973af02cf4e5
Dhanush Adithyan <dhanushvallalar@gmail.com>
2026-03-15 16:26:07 +0530

    Readme update

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,5 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+import { resolve } from "path";
+
+dotenv.config({ path: resolve(__dirname, "../../.env") });
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -11,6 +15,13 @@ const config: HardhatUserConfig = {
       },
       viaIR: true,
       evmVersion: "cancun",
+    },
+  },
+  networks: {
+    goat_testnet: {
+      url: process.env.GOAT_RPC_URL || "https://rpc.testnet3.goat.network",
+      chainId: 48816, // 0xBEB0
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
